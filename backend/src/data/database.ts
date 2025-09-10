d80f62ffe28a41cea9b4e44364477b42b59d05b8
Ruan Finamor <ruanfinamor@gmail.com>
2025-09-10 10:40:13 -0300

    REFACTOR: Adding .env to project

--- backend/src/data/database.ts
+++ backend/src/data/database.ts
@@ -1,15 +1,17 @@
 import sqlite3 from 'sqlite3';
 import path from 'path';
+import dotenv from 'dotenv';
 
-const dbDir = path.join(__dirname, '..', '..');
-const dbPath = path.join(dbDir, 'database.sqlite');
+dotenv.config();
+
+const dbPath = path.join(__dirname, process.env.DB_PATH || '../../database.sqlite');
 
 const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
     console.error('Error opening database:', err.message);
     console.error('Database path:', dbPath);
   } else {
-    console.log('Connected to the SQLite database.');    
+    console.log('Connected to the SQLite database at', dbPath);    
     
     db.run(`
       CREATE TABLE IF NOT EXISTS visited_countries (
